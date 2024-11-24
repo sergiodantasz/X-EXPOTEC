5755650f5d4abd32c7d16b5d5a7b77cf54bb9afb
vitor.belisio <a77joao88vitorz@gmail.com>
2024-11-24 15:38:15 -0300

    finalização do front-end

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const bodyD = document.querySelector('body');
+const pop_up = document.querySelector(".pop_up");
+const grupos = document.querySelectorAll(".arq_btn");
+const btnVoltar = document.getElementById('voltar');
 
+grupos.forEach(grupo => {
+    grupo.onclick = () => {
+        pop_up.classList.remove('voltar');
+        pop_up.style.display = 'flex';
+    };
+});
+
+btnVoltar.onclick = () => {
+    pop_up.classList.add('voltar');
+    setTimeout(() => {
+        pop_up.style.display = 'none';
+    }, 300);
+};
+
+const LeftScroll = document.getElementById('right_slider');
+const RightScroll = document.getElementById('left_slider')
+const GrupoUM = document.querySelector('.gp1');
+var margem = 0;
+
+LeftScroll.onclick = () => {
+    GrupoUM.style.marginLeft = '-9.3%';
+    LeftScroll.classList.add('voltar');
+    RightScroll.style.display = 'flex';
+    setTimeout(() => {
+        LeftScroll.style.display = 'none';
+    }, 301);
+}
+RightScroll.onclick = () => {
+    GrupoUM.style.marginLeft = '0';
+    RightScroll.classList.add('voltar');
+    LeftScroll.style.display = 'flex';
+    setTimeout(() => {
+        RightScroll.style.display = 'none';
+    }, 301);
+}
+
+ScrollReveal().reveal('.hero');
+ScrollReveal().reveal('.gp1', { delay: 300 });
+ScrollReveal().reveal('.gp2', { delay: 400 });
+ScrollReveal().reveal('.gp3', { delay: 500 });
+ScrollReveal().reveal('.gp4', { delay: 600 });
+ScrollReveal().reveal('.gp5', { delay: 700 });
